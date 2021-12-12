2786e04a341ca8f930cb5e9ba456b418998a333c
bshien <bshien5124@gmail.com>
2021-12-12 15:34:17 -0800

    Fixed some bugs

--- forecasttest/interfacetest.py
+++ forecasttest/interfacetest.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import geocoder
 from forecastiopy import *
+import time
 apikey = 'e189b55a5cea6512bed42aca3e9e3aa4'
 tempdata = []
 dayindex = []
@@ -19,17 +20,17 @@ def geocode():
     print('Timezone', fio.timezone, 'Offset', fio.offset)
     print(fio.get_url())  # You might want to see the request url
 
-    """if fio.has_currently() is True:
-        currently = FIOCurrently.FIOCurrently(fio)
-        print('Currently')
-        # for item in currently.get().keys():
-        # print(item + ' : ' + unicode(currently.get()[item]))
-        # Or access attributes directly
-        print(currently.temperature)
-        print(currently.humidity)
-    else:
-        print('No Currently data')
-        """
+    # if fio.has_currently() is True:
+    #     currently = FIOCurrently.FIOCurrently(fio)
+    #     print('Currently')
+    #     # for item in currently.get().keys():
+    #     # print(item + ' : ' + unicode(currently.get()[item]))
+    #     # Or access attributes directly
+    #     print(currently.temperature)
+    #     print(currently.humidity)
+    # else:
+    #     print('No Currently data')
+
     if fio.has_hourly() is True:
         hourly = FIOHourly.FIOHourly(fio)
         print('Hourly')
@@ -46,17 +47,33 @@ def geocode():
         # print(hourly.hour_49_temperature)
 
         for hour in range(0, hourly.hours()):
-
-            print('Hour', hour + 1)
-            print('Temperature:' + str(hourly.get_hour(hour)['temperature']))
+        #
+        #     print('Hour', hour + 1)
+        #     print('Temperature:' + str(hourly.get_hour(hour)['temperature']))
             tempdata.append(str(hourly.get_hour(hour)['temperature']))
              #Or access attributes directly for a given minute.
              #hourly.hour_5_time would also work
 
-        print(hourly.hour_49_time)
-        for x in tempdata:
-            print(x)
+        # print(hourly.hour_49_time)
+        # for x in tempdata:
+        #     print(x)
         indexcalc()
+        if dayindex:
+            t = time.localtime()
+            cur_hour = t[3]
+            run_hour = cur_hour + dayindex[0]
+            display_run_hour = run_hour % 12
+            run_day = run_hour // 24
+            am = True
+            if (run_hour // 12) % 2 == 1:
+                am = False
+            if run_day == 0:
+                print("You can run at", display_run_hour, " o'clock ", "AM" if am else "PM")
+            else:
+                print("You can run in ", run_day, " days at ", display_run_hour, " o'clock ", "AM" if am else "PM")
+        else:
+            print("You cannot run in the next 50 hours, try choosing a higher temperature.")
+
 
 
 
@@ -64,11 +81,15 @@ def geocode():
         print('No Hourly data')
 
 def indexcalc():
-    for x in tempdata:
-        if x <= e3.get():
-            dayindex.append(tempdata.index(x))
-    for x in dayindex:
-        print(x)
+    for x in range(0, len(tempdata)):
+        if tempdata[x] <= e3.get():
+            dayindex.append(x + 1)
+
+    # print("----------------------------------------------------")
+    #
+    # Below code prints out each hour in the future that is less than target temperature up to 50 hrs
+    # for x in dayindex:
+    #     print(x)
 
 
 
